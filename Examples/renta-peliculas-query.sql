0eab1d292a6ba9982813b9bf75bd4fe2fac2f820
Nicolás Mayoral <n.mayoral@alumno.um.edu.ar>
2022-08-29 20:08:05 -0300

    subqueries para rent films sql, sin join

--- Examples/renta-peliculas-query.sql
+++ Examples/renta-peliculas-query.sql
@@ -11,14 +11,17 @@ SELECT * FROM customer;
 
 SELECT * FROM staff;
 
+SELECT * FROM address;
+
+SELECT * FROM payment;
+
 # CREACION DE VISTA RENT FILMS - SUB CONSULTA INNER JOIN
 CREATE VIEW RentFilms AS
 SELECT F.title as "Film Title",
 		F.description as "Film Description",
         F.release_year as "Release Year",
         S.username as "Seller's name",
-        C.first_name as "Client's First name",
-        C.last_name as "Client's Last name",
+		concat(C.first_name, ' ', C.last_name) as "Client",
         C.email as "Client email",
         R.rental_date as "Rental Date",
         R.return_date as "Return Date"
@@ -36,8 +39,7 @@ SELECT F.title as "Film Title",
 		F.description as "Film Description",
         F.release_year as "Release Year",
         S.username as "Seller's name",
-        C.first_name as "Client's First name",
-        C.last_name as "Client's Last name",
+        concat(C.first_name, ' ', C.last_name) as "Client",
         C.email as "Client email",
         R.rental_date as "Rental Date",
         R.return_date as "Return Date"
@@ -47,4 +49,20 @@ INNER JOIN rental R ON (I.inventory_id = R.inventory_id)
 INNER JOIN customer C ON (C.customer_id = R.customer_id)
 INNER JOIN staff S ON (S.staff_id = R.staff_id)
 WHERE F.title LIKE "Y%";
+
+# SUBQUERY
+SELECT 
+
+	R.rental_date as "Rental Date",
+    R.return_date as "Return Date",
+    (SELECT concat(C.first_name, ' ', C.last_name) FROM customer C WHERE C.customer_id = R.customer_id) "Client",
+    (SELECT C.email FROM customer C WHERE C.customer_id = R.customer_id) "Email",
+    (SELECT concat(A.address, ' - ', A.district) FROM address A WHERE A.address_id = (SELECT C.address_id FROM customer C WHERE C.customer_id = R.customer_id)) "Address",
+    (SELECT A.phone FROM address A WHERE A.address_id = (SELECT C.address_id FROM customer C WHERE C.customer_id = R.customer_id)) "Phone",
+    P.staff_id "Code Seller - Payment",
+    R.staff_id "Code Seller - Rent"
+	
+FROM rental R,
+	payment P
+    WHERE R.rental_id = P.rental_id;
 
