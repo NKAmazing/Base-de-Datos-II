18702a6b1a7b7754f296afdadaa998a533be4437
Nicolas <n.mayoral@alumno.um.edu.ar>
2022-09-05 15:24:34 -0300

    join y subconsulta select terminados

--- Examples/renta-peliculas-query.sql
+++ Examples/renta-peliculas-query.sql
@@ -20,16 +20,19 @@ CREATE VIEW RentFilms AS
 SELECT F.title as "Film Title",
 		F.description as "Film Description",
         F.release_year as "Release Year",
-        S.username as "Seller's name",
+        concat(S.first_name, ' ', S.last_name)  as "Seller's name",
 		concat(C.first_name, ' ', C.last_name) as "Client",
         C.email as "Client email",
+        A.address as "Client Address",
+        A.phone as "Client Phone",
         R.rental_date as "Rental Date",
         R.return_date as "Return Date"
  FROM film F 
 INNER JOIN inventory I ON (F.film_id = I.film_id) 
 INNER JOIN rental R ON (I.inventory_id = R.inventory_id)
 INNER JOIN customer C ON (C.customer_id = R.customer_id)
-INNER JOIN staff S ON (S.staff_id = R.staff_id);
+INNER JOIN staff S ON (S.staff_id = R.staff_id)
+INNER JOIN address A ON (A.address_id = C.address_id);
 
 # Seleccionamos la Vista
 SELECT * FROM RentFilms;
@@ -38,9 +41,11 @@ SELECT * FROM RentFilms;
 SELECT F.title as "Film Title",
 		F.description as "Film Description",
         F.release_year as "Release Year",
-        S.username as "Seller's name",
+        concat(S.first_name, ' ', S.last_name)  as "Seller's name",
         concat(C.first_name, ' ', C.last_name) as "Client",
         C.email as "Client email",
+        A.address as "Client Address",
+        A.phone as "Client Phone",
         R.rental_date as "Rental Date",
         R.return_date as "Return Date"
  FROM film F 
@@ -48,20 +53,20 @@ INNER JOIN inventory I ON (F.film_id = I.film_id)
 INNER JOIN rental R ON (I.inventory_id = R.inventory_id)
 INNER JOIN customer C ON (C.customer_id = R.customer_id)
 INNER JOIN staff S ON (S.staff_id = R.staff_id)
-WHERE F.title LIKE "Y%";
+INNER JOIN address A ON (A.address_id = C.address_id);
 
 # SUBQUERY
 SELECT 
-
+	(SELECT F.title FROM film F WHERE F.film_id = (SELECT I.film_id FROM inventory I WHERE I.inventory_id = R.inventory_id)) "Film Title",
 	R.rental_date as "Rental Date",
     R.return_date as "Return Date",
     (SELECT concat(C.first_name, ' ', C.last_name) FROM customer C WHERE C.customer_id = R.customer_id) "Client",
-    (SELECT C.email FROM customer C WHERE C.customer_id = R.customer_id) "Email",
-    (SELECT concat(A.address, ' - ', A.district) FROM address A WHERE A.address_id = (SELECT C.address_id FROM customer C WHERE C.customer_id = R.customer_id)) "Address",
-    (SELECT A.phone FROM address A WHERE A.address_id = (SELECT C.address_id FROM customer C WHERE C.customer_id = R.customer_id)) "Phone",
+    (SELECT C.email FROM customer C WHERE C.customer_id = R.customer_id) "Client Email",
+    (SELECT concat(A.address, ' - ', A.district) FROM address A WHERE A.address_id = (SELECT C.address_id FROM customer C WHERE C.customer_id = R.customer_id)) "Client Address",
+    (SELECT A.phone FROM address A WHERE A.address_id = (SELECT C.address_id FROM customer C WHERE C.customer_id = R.customer_id)) "Client Phone",
+    (SELECT concat(S.first_name, ' ', S.last_name) FROM staff S WHERE S.staff_id = R.staff_id) "Seller's name",
     P.staff_id "Code Seller - Payment",
     R.staff_id "Code Seller - Rent"
-	
 FROM rental R,
 	payment P
     WHERE R.rental_id = P.rental_id;
